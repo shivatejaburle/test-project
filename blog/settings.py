40313daa3866ae1a16442608a74e18a2ca4e3277
Shivateja Burle <shivateja.idizital@gmail.com>
2024-08-01 13:38:58 +0530

    Update settings.py
    DEBUG=False

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)7mrl+wp2mh*ytgal0h+p%1h)5vgbo_d&voh$@o*_zg(uevl4k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
